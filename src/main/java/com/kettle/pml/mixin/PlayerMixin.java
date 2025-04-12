5b8e62b9c1417ebadfbecf1a3d1acf2accc020e1
Kettle5000 <93736713+Kettle5000@users.noreply.github.com>
2025-04-12 12:13:08 -0400

    Update PlayerMixin.java

--- src/main/java/com/kettle/pml/mixin/PlayerMixin.java
+++ src/main/java/com/kettle/pml/mixin/PlayerMixin.java
@@ -4,12 +4,10 @@ import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import com.kettle.pml.core.PMLDamageTypes;
 import com.llamalad7.mixinextras.sugar.Local;
@@ -18,17 +16,23 @@ import com.llamalad7.mixinextras.sugar.Local;
 public class PlayerMixin {
 
 	//Aims for Flag5 so when dealing a critical hit, damage is done as one
-	@Redirect(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z", ordinal = 0))
-	private boolean modifyDamageFlag(Entity target, DamageSource originalSource, float originalDamage, @Local(ordinal = 2) boolean flag2) {
+	@ModifyArg(
+	        method = "attack", 
+	        at = @At(
+	            value = "INVOKE", 
+	            target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z", ordinal = 0), index = 0, remap = false)
+	private DamageSource modifyDamageFlag(DamageSource original, @Local(ordinal = 2) boolean flag2) {
 		Player player = getThis();
 		if (flag2) {
-		    DamageSource critical_damage = new DamageSource(PMLDamageTypes.getDamageType(player.level(), PMLDamageTypes.PLAYER_CRITICAL_KEY), player);
-			return target.hurt(critical_damage, originalDamage);
+		    return new DamageSource(
+					PMLDamageTypes.getDamageType(player.level(), PMLDamageTypes.PLAYER_CRITICAL_KEY), player);
+		} else {
+			return original;
 		}
-		return target.hurt(originalSource, originalDamage);
 	}
 	
-	@ModifyArg(method = {"attack"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z", ordinal = 0), index = 0)
+	@ModifyArg(method = {
+			"attack" }, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z", ordinal = 0), index = 0, remap = false)
 	private DamageSource modifySweepAttackDamage(DamageSource original) {
 		Player player = getThis();
 		if (original.is(DamageTypes.PLAYER_ATTACK)) {
